90b423722119b1ee95e0243bfc81019b9ed47625
Rafael <rafarafa99944@gmail.com>
2026-06-14 20:28:20 -0300

    Revert "Adding notebooks to repository"
    This reverts commit 00d70b31734fa0dad7dbb628d910e2c3201f5d4f.

--- 03_gold.sql
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC
-# MAGIC CREATE SCHEMA IF NOT EXISTS spotify_catalog.bronze;
-# MAGIC CREATE SCHEMA IF NOT EXISTS spotify_catalog.silver;
-# MAGIC CREATE SCHEMA IF NOT EXISTS spotify_catalog.gold;
-# MAGIC
